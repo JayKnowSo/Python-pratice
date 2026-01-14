db1507fc69c6b5661d5d409898f3347b34965038
jayknowso <worklife0524@gmail.com>
2026-01-14 02:05:52 -0500

    added simple decision example

---
+++ Decision.py
@@ -0,0 +1,8 @@
+# Notes: Thos program makes a simple decision
+# Uses input, variables and if/ else
+# Builds on the basics 1
+name = input("Enter your name:")
+if name == "James":
+    print(f"Good mourning, {name}!")
+else: 
+    print("Good mourning, guest!")
--- What_a_time_to_be_alive.py
+++ What_a_time_to_be_alive.py
@@ -1,1 +1,4 @@
+# Notes: print () displays text on the screen
+# f-strings allow variables to be placed inside of text
+# curly braces {} show where the variable goes
 print (f"Wake up, Alice! Wonderland is calling through the rabbit holes!")
