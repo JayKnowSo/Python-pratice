1b7358a96484854f16b021f44887b237eccffdde
jayknowso <worklife0524@gmail.com>
2026-01-14 02:30:47 -0500

    add clean functions example

---
+++ Functions_clean.py
@@ -0,0 +1,9 @@
+# Notes: Functions group code into a reusable blocks
+# Functions run only when called
+# Parameters recieve values when the function is called
+
+def greet(name):
+    print(f"Good afternoon, {name}!")
+
+greet("Madison")
+greet("Jemel") 
